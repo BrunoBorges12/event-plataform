dc392a1c954d8a9f7aa3e08f811e26db9aec403c
Bruno dos santos <brunoamix084@gmail.com>
2022-06-20 23:40:46 -0300

    começo de projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './style/global.css'
+
+function App() {
+
+  return (
+  
+  <h1 className=' bg-slate-100 text-2xl font-bold'>Obrigado Deus</h1>
+  )
+}
+
+export default App
